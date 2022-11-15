f4abe56ab0861ec76942d610425c306de60f1567
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-11-15 23:52:04 +0100

    working on the sum array

---
+++ src/test/java/hackerRank/SumArrayTest.java
@@ -0,0 +1,19 @@
+package hackerRank;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SumArrayTest {
+
+    private SumArray sumArray;
+
+    @BeforeEach
+    public void initSetup(){
+        sumArray = new SumArray();
+    }
+
+    @Test
+    public void test_test(){
+    }
+}
