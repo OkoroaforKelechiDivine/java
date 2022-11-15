e33fb57719e73f375cb356bc81819bc18c04583c
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-11-16 00:24:16 +0100

    done with sum array

--- src/main/java/hackerRank/SumArray.java
+++ src/main/java/hackerRank/SumArray.java
@@ -1,4 +1,25 @@
 package hackerRank;
 
+import java.util.List;
+
 public class SumArray {
+
+    public int simpleArraySum(int[] numberArray){
+        int result = 0;
+
+        for (int index : numberArray) {
+            result = result + index;
+            System.out.println("The sum of the array is: " + result);
+        }
+        return result;
+    }
+
+    public int simpleArraySumList(List<Integer> ar){
+        int result = 0;
+
+        for (int index :  ar){
+            result = result + index;
+        }
+        return result;
+    }
 }
--- src/test/java/hackerRank/SumArrayTest.java
+++ src/test/java/hackerRank/SumArrayTest.java
@@ -1,8 +1,12 @@
 package hackerRank;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SumArrayTest {
 
@@ -15,5 +19,20 @@ public class SumArrayTest {
 
     @Test
     public void test_test(){
+        int[] numbers = {3, 4, 5};
+        int sum = sumArray.simpleArraySum(numbers);
+        Assertions.assertEquals(sum, 12);
+    }
+
+    @Test
+    public void test_listArray(){
+        List<Integer> listOfNumbers = new ArrayList<>();
+        listOfNumbers.add(3);
+        listOfNumbers.add(5);
+        listOfNumbers.add(3);
+        listOfNumbers.add(1);
+
+        int sum = sumArray.simpleArraySumList(listOfNumbers);
+        Assertions.assertEquals(sum, 12);
     }
 }
