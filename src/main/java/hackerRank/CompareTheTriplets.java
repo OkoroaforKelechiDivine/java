741a266114384c6e09db8e36f318603dc784e48e
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-11-16 03:57:15 +0100

    working on grade score

--- src/main/java/hackerRank/CompareTheTriplets.java
+++ src/main/java/hackerRank/CompareTheTriplets.java
@@ -1,22 +1,34 @@
 package hackerRank;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class CompareTheTriplets {
 
-    public int compareElement(int[] aliceGrade, int[] bobGrade) throws Exception {
+    public List<Integer> compareElement(List<Integer> aliceGrade, List<Integer> bobGrade) throws Exception {
 
-        int gradePoint = 0;
+//        A score list that will store Alice and Bob's score
+        List<Integer> scores = new ArrayList<>();
 
-        if (aliceGrade.length != 3 && bobGrade.length != 3){
-            throw new Exception("Grade length has to be 3");
+        int aliceScore = 0;
+        int bobScore = 0;
+
+//        I will use Alice's score size to determine Bob's score size too!
+        for (int index = 0; index < aliceGrade.size(); index++){
+            if(aliceGrade.get(index) > bobGrade.get(index)){
+                aliceScore += 1;
+            }
+            if (Objects.equals(aliceGrade.get(index), bobGrade.get(index))){
+                System.out.println("Can not grade this two grade at: " + aliceGrade.get(index) + " and " + bobGrade.get(index));
+            }
+            else if(bobGrade.get(index) > aliceGrade.get(index)) {
+                bobScore += 1;
+            }
         }
+        scores.add(aliceScore);
+        scores.add(bobScore);
 
-        if (aliceGrade[0] > bobGrade[0]){
-            gradePoint = gradePoint + 1;
-        }
-
-        if (bobGrade[0] > aliceGrade[0]){
-            gradePoint = gradePoint + 1;
-        }
-        return gradePoint;
+        return scores;
     }
 }
--- src/test/java/hackerRank/CompareTheTripletsTest.java
+++ src/test/java/hackerRank/CompareTheTripletsTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompareTheTripletsTest {
 
     private CompareTheTriplets compareTheTriplets;
@@ -15,10 +18,66 @@ public class CompareTheTripletsTest {
 
     @Test
     public void test_thatAliceGotOnePointAtIndex0() throws Exception {
-        int [] aliceGrade = {4, 3, 4};
-        int [] bobGrade = {1, 56, 7};
-        int result = compareTheTriplets.compareElement(aliceGrade, bobGrade);
-        Assertions.assertEquals(result, 1);
-    }
+        List<Integer> aliceScore = new ArrayList<>();
+        List<Integer> bobScore = new ArrayList<>();
+        List<Integer> scores = new ArrayList<>();
 
+        aliceScore.add(3);
+        aliceScore.add(2);
+        aliceScore.add(1);
+
+        bobScore.add(2);
+        bobScore.add(6);
+        bobScore.add(1);
+        scores = compareTheTriplets.compareElement(aliceScore, bobScore);
+        Assertions.assertNotNull(scores);
+    }
+//
+//    @Test
+//    public void test_thatBobGotOnePointAtIndex0() throws Exception {
+//        int [] aliceGrade = {2, 3, 4};
+//        int [] bobGrade = {15, 56, 7};
+//        int result = compareTheTriplets.compareElement(aliceGrade, bobGrade);
+//        Assertions.assertEquals(result, 1);
+//    }
+//
+//    @Test
+//    public void test_thatAliceGotOnePointAtIndex1() throws Exception {
+//        int [] aliceGrade = {2, 3, 4};
+//        int [] bobGrade = {15, 1, 7};
+//        int result = compareTheTriplets.compareElement(aliceGrade, bobGrade);
+//        Assertions.assertEquals(result, 2);
+//    }
+//
+//    @Test
+//    public void test_thatBobGotOnePointAtIndex1() throws Exception {
+//        int [] aliceGrade = {2, 3, 4};
+//        int [] bobGrade = {15, 10, 7};
+//        int result = compareTheTriplets.compareElement(aliceGrade, bobGrade);
+//        Assertions.assertEquals(result, 2);
+//    }
+//
+//    @Test
+//    public void test_thatAliceGotOnePointAtIndex2() throws Exception {
+//        int [] aliceGrade = {2, 3, 24};
+//        int [] bobGrade = {15, 10, 7};
+//        int result = compareTheTriplets.compareElement(aliceGrade, bobGrade);
+//        Assertions.assertEquals(result, 3);
+//    }
+//
+//    @Test
+//    public void test_thatBobGotOnePointAtIndex2() throws Exception {
+//        int [] aliceGrade = {2, 3, 4};
+//        int [] bobGrade = {15, 10, 7};
+//        int result = compareTheTriplets.compareElement(aliceGrade, bobGrade);
+//        Assertions.assertEquals(result, 3);
+//    }
+//
+//    @Test
+//    public void test_thatBothAliceAndBobGotNoPointIfGradeIsEqual() throws Exception {
+//        int [] aliceGrade = {2, 3, 7};
+//        int [] bobGrade = {2, 10, 7};
+//        int result = compareTheTriplets.compareElement(aliceGrade, bobGrade);
+//        Assertions.assertEquals(result, 1);
+//    }
 }
