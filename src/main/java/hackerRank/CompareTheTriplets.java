775d735c2c45909145ca21c2954a30364b938607
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-11-16 01:39:36 +0100

    working on grade score

--- src/main/java/hackerRank/CompareTheTriplets.java
+++ src/main/java/hackerRank/CompareTheTriplets.java
@@ -2,5 +2,21 @@ package hackerRank;
 
 public class CompareTheTriplets {
 
+    public int compareElement(int[] aliceGrade, int[] bobGrade) throws Exception {
 
+        int gradePoint = 0;
+
+        if (aliceGrade.length != 3 && bobGrade.length != 3){
+            throw new Exception("Grade length has to be 3");
+        }
+
+        if (aliceGrade[0] > bobGrade[0]){
+            gradePoint = gradePoint + 1;
+        }
+
+        if (bobGrade[0] > aliceGrade[0]){
+            gradePoint = gradePoint + 1;
+        }
+        return gradePoint;
+    }
 }
--- src/test/java/hackerRank/CompareTheTripletsTest.java
+++ src/test/java/hackerRank/CompareTheTripletsTest.java
@@ -1,5 +1,24 @@
 package hackerRank;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CompareTheTripletsTest {
+
+    private CompareTheTriplets compareTheTriplets;
+
+    @BeforeEach
+    public void initSetup(){
+        compareTheTriplets = new CompareTheTriplets();
+    }
+
+    @Test
+    public void test_thatAliceGotOnePointAtIndex0() throws Exception {
+        int [] aliceGrade = {4, 3, 4};
+        int [] bobGrade = {1, 56, 7};
+        int result = compareTheTriplets.compareElement(aliceGrade, bobGrade);
+        Assertions.assertEquals(result, 1);
+    }
 
 }
